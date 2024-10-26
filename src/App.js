0788f6c2c1e8ff4d85fb1d1e0e139aa824f8be60
Sumedh Sinha <sumedhsinha2022@gmail.com>
2024-10-26 19:45:01 -0400

    fixes previous issues and add gemini

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { getHelloMessage } from './apiService';
-import './App.css'; // Ensure your CSS file is imported
+import './App.css';
 
 function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    getHelloMessage().then((data) => setMessage(data.message));
+    getHelloMessage()
+      .then((data) => setMessage(data.message))
+      .catch(() => setMessage("Error loading message"));
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
+        <h2>
+          Our mission is to enhance learning and improve patient care by providing medical students with AI-driven insights on their clinical documentation. We aim to streamline the grading process and deliver constructive feedback, fostering the development of clinical communication.
+        </h2>
         <p>{message ? message : 'Loading...'}</p>
         <div className="button-container">
           <button className="button" id="teacherButton">Teacher</button>
---
+++ src/backend.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello_world():
+    return '<p>Hello, World!</p>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
