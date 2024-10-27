1e577dcd89b93253cdacf4da43304b8deb55af9c
r.r.dhi <dhingrabusiness@gmail.com>
2024-10-26 21:34:04 -0400

    this is style

--- src/App.js
+++ src/App.js
@@ -8,19 +8,23 @@ function App() {
   useEffect(() => {
     getHelloMessage()
       .then((data) => setMessage(data.message))
-      .catch(() => setMessage("Error loading message"));
+      .catch(() => setMessage("Select an option"));
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h2>
+        <div className="logo-container">
+          <img src="logo.png" alt="Logo 1" className="logo" />      
+        </div>
+        <h2 className="subtitle">Welcome to CureBYTES!</h2>
+        <p className="description">
           Our mission is to enhance learning and improve patient care by providing medical students with AI-driven insights on their clinical documentation. We aim to streamline the grading process and deliver constructive feedback, fostering the development of clinical communication.
-        </h2>
-        <p>{message ? message : 'Loading...'}</p>
+        </p>
+        <p className="status-message">{message ? message : 'Select an option'}</p>
         <div className="button-container">
-          <button className="button" id="teacherButton">Teacher</button>
-          <button className="button" id="studentButton">Student</button>
+          <button className="button" id="teacherButton">🍎  Teacher</button>
+          <button className="button" id="studentButton">📚 Student</button>
         </div>
       </header>
     </div>
